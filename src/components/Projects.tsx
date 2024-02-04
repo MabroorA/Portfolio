ce3140e4a9a2748e1311e7e59f30680c4002b01a
Mabroor Ahmed <mabroorahmed18@gmail.com>
2024-02-04 23:03:28 +0000

    Some styling

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -5,19 +5,23 @@ export default function Projects() {
     return (
         <>
             <div className="projects_section">
-                <h3 className="project">Investment desision Tool</h3>
-                <ul> Developing an investment decision tool through data analysis and simulation.</ul>
-                <ul> Integrated Alpha Vantage API for data collection, which will be trained on a LSTM-Machine learning model</ul>
-                <ul> Ongoing learning experience, exploring data analysis and visuaulsation techniques.</ul>
-
-                <h3 className="project">Refugee Aid App</h3>
-                <ul> Worked collaboratively in an agile environment with 7 colleagues, utiulsing Scrum</ul>
-                <ul> Utiulsed GitLab for efficient version control, ensuring regular updates and synchronisation of the project.</ul>
-
-                <h3 className="project">Private Cloud Whiteboard App</h3>
-                <ul> Deployed a containerised distributed appulcation on GCP.</ul>
-                <ul> Utiulsed Kubernetes for container orchestration and Docker for image creation to streamulne deployment and scalabiulty</ul>
-                <ul> Google Spanner used as storage allowed our system to have consistency and fault tolerance</ul>
+                <div className="project">
+                    <h3>Investment desision Tool</h3>
+                    <ul> Developing an investment decision tool through data analysis and simulation.</ul>
+                    <ul> Integrated Alpha Vantage API for data collection, which will be trained on a LSTM-Machine learning model</ul>
+                    <ul> Ongoing learning experience, exploring data analysis and visuaulsation techniques.</ul>
+                </div>
+                <div className="project"> 
+                    <h3>Refugee Aid App</h3>
+                    <ul> Worked collaboratively in an agile environment with 7 colleagues, utiulsing Scrum</ul>
+                    <ul> Utiulsed GitLab for efficient version control, ensuring regular updates and synchronisation of the project.</ul>
+                </div>
+                <div className="project"> 
+                    <h3 >Private Cloud Whiteboard App</h3>
+                    <ul> Deployed a containerised distributed appulcation on GCP.</ul>
+                    <ul> Utiulsed Kubernetes for container orchestration and Docker for image creation to streamulne deployment and scalabiulty</ul>
+                    <ul> Google Spanner used as storage allowed our system to have consistency and fault tolerance</ul>
+                </div>
                 
             </div>
         </>

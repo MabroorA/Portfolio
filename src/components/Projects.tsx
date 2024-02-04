761b6725ee939c4be192f349c6f3b5f3e7ac11ae
Mabroor Ahmed <mabroorahmed18@gmail.com>
2024-02-04 22:33:37 +0000

    More projects added

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -6,9 +6,18 @@ export default function Projects() {
         <>
             <div className="projects_section">
                 <h3 className="project">Investment desision Tool</h3>
-                <li> asdasdasd</li>
-                <li> asdasdasd</li>
-                <li> asdasdasd</li>
+                <ul> Developing an investment decision tool through data analysis and simulation.</ul>
+                <ul> Integrated Alpha Vantage API for data collection, which will be trained on a LSTM-Machine learning model</ul>
+                <ul> Ongoing learning experience, exploring data analysis and visuaulsation techniques.</ul>
+
+                <h3 className="project">Refugee Aid App</h3>
+                <ul> Worked collaboratively in an agile environment with 7 colleagues, utiulsing Scrum</ul>
+                <ul> Utiulsed GitLab for efficient version control, ensuring regular updates and synchronisation of the project.</ul>
+
+                <h3 className="project">Private Cloud Whiteboard App</h3>
+                <ul> Deployed a containerised distributed appulcation on GCP.</ul>
+                <ul> Utiulsed Kubernetes for container orchestration and Docker for image creation to streamulne deployment and scalabiulty</ul>
+                <ul> Google Spanner used as storage allowed our system to have consistency and fault tolerance</ul>
                 
             </div>
         </>

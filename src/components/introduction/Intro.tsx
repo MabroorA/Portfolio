40dda622e550194f7177a6ad36e9bac9ca1bab23
Mabroor Ahmed <mabroorahmed18@gmail.com>
2024-02-09 18:53:06 +0000

    layout complete

--- src/components/introduction/Intro.tsx
+++ src/components/introduction/Intro.tsx
@@ -6,16 +6,15 @@ import "./Intro.css";
 export default function Intro() {
   return (
     <>
-      <div className="container">
+      <div className="intro-container">
         <div className="intro">
           <h1>Mabroor Ahmed</h1>
-          <h6>Leicester,England</h6>
+          <p>Leicester,England</p>
         </div>
         <div className="contact-details">
           <a href="mabroorahmed2@gmail.com">Mabroorahmed2@gmail.com</a>
           <a href="https://www.linkedin.com/in/mabroor-dev/">Linkedin</a>
           <a href="https://github.com/MabroorA">Github</a>
-          <p>Logos will go here</p>
         </div>
       </div>
     </>

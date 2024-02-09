f20e589334cf7a6a0ac4750727a9dbdd863193a6
Mabroor Ahmed <mabroorahmed18@gmail.com>
2024-02-09 13:16:58 +0000

    component strcuture

--- src/components/introduction/Intro.tsx
+++ src/components/introduction/Intro.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa6";
-import "../App.css";
+import "./Intro.css";
 
 export default function Intro() {
   return (
     <>
-      <div className="intro_section">
+      <div className="intro">
         <h1 className="Name">Mabroor Ahmed</h1>
         <h6>Leicester,England</h6>
       </div>
-      <div className="social_links">
+      <div className="contact-details">
         <a href="mabroorahmed2@gmail.com">Mabroorahmed2@gmail.com</a>
         <a href="https://www.linkedin.com/in/mabroor-dev/">Linkedin</a>
         <a href="https://github.com/MabroorA">Github</a>

792570bca20990d6822681711f32d025a1a5fc6a
Mabroor Ahmed <mabroorahmed18@gmail.com>
2024-02-11 23:33:49 +0000

    menu icon

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,17 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
+
 import pfp from "./pfp.jpg";
 import "./Menu.css";
 
 export default function Menu() {
-  const menulist = [
-    "web dev @ Minibikers",
-    "Uni of Leicester & WQE ",
-    "Project list will go here ",
-    "typescript",
-  ];
-
-  menulist.map((item) => <li>{item}</li>);
-
+  const [menutoggle, Setmenutoggle] = useState(false);
   return (
     <>
       <div className="sidenav">
@@ -19,6 +13,9 @@ export default function Menu() {
         <a href="#experience">Experience</a>
         <a href="#Projects">Projects</a>
         <a href="#Education">Education</a>
+        <a href="" onClick={() => Setmenutoggle(true)} className="menu-icon">
+          <MenuRoundedIcon />
+        </a>
       </div>
     </>
   );

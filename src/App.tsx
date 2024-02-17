ea657bc8d2307ab5fd075615184b2944ad75e4ff
Mabroor Ahmed <mabroorahmed18@gmail.com>
2024-02-17 00:46:18 +0000

    unused import removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import Projects from "./components/projects/Projects";
 import Menu from "./components/menu/Menu";
 import Intro from "./components/introduction/Intro";
-import Experience from "./components/experience/Experience";
 import "./App.css";
 function App() {
   return (

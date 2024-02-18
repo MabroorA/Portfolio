4d097f0e5b0617cf574fed38979115329cbe7ab2
Mabroor Ahmed <mabroorahmed18@gmail.com>
2024-02-18 01:49:47 +0000

    added burger menu for mobile

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,17 +1,30 @@
+import { useState } from "react";
+import { RiMenu4Fill } from "react-icons/ri";
+
 import "./Menu.css";
 
 export default function Menu() {
+  const [isOpen,SetisOpen] = useState(false)
+
+  const toggleMenu = () => {
+    SetisOpen((open) => !open);
+  }; 
   return (
     <>
       <div className="menu">
         <a className="name" href="/">
           Mabroor Ahmed
         </a>
-        <div className="rlist">
-          <a href="/">Experience</a>
-          <a href="#projects">Projects</a>
-          <a href="/Education">Education</a>
+        <div className={`rlist ${isOpen ? "is-open" : ""}`}>
+          <div className="rlist">
+            <a href="/">Experience</a>
+            <a href="#projects">Projects</a>
+            <a href="/Education">Education</a>
+          </div>
         </div>
+      </div>
+      <div className="hamburger" onClick={toggleMenu}>
+        <RiMenu4Fill />
       </div>
       <div className="scroll-watcher"></div> {/* Scroll bar */}
     </>

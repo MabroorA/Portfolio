e9c700f97b0af2a76a5129328c8bba9684aef874
Mabroor Ahmed <mabroorahmed18@gmail.com>
2024-02-22 21:23:14 +0000

    tech stack made static

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import Menu from "./components/menu/Menu";
 import Intro from "./components/introduction/Intro";
 import TechStack from "./components/techStack/techstack";
 import "./App.css";
+import Slidingwindow from "./components/techStack/Slidingwindow";
 function App() {
   return (
     <>
       <div className="main">
         <Menu/>
         <Intro />
-        <TechStack/>
+        <Slidingwindow/>
+        {/* <TechStack/> */}
         <Projects />
         {/* <Experience /> */}
       </div>
---
+++ src/components/techStack/Slidingwindow.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { BiLogoKubernetes, BiLogoSpringBoot } from 'react-icons/bi'
+import { DiMysql } from 'react-icons/di'
+import { FaNodeJs, FaReact } from 'react-icons/fa'
+import { GrDocker } from 'react-icons/gr'
+import "./Slidingwindow.css"
+
+export default function Slidingwindow() {
+  return (
+    <>
+
+      <div className="slidingwindow">
+        <div className="slide-item">
+          <ul className="react">
+            <FaReact size={33} /> React
+          </ul>
+          <ul className="nodejs">
+            <FaNodeJs size={33} /> Nodejs
+          </ul>
+          <ul className="mysql">
+            {" "}
+            <DiMysql size={33} /> My<span className="sql">SQL</span>
+          </ul>
+          <ul className="springboot">
+            <BiLogoSpringBoot size={33} /> Spring Boot
+          </ul>
+          <ul className="docker">
+            <GrDocker size={33} /> Docker
+          </ul>
+          <ul className="kubernetes">
+            <BiLogoKubernetes size={33} /> Kubernetes
+          </ul>
+        </div>
+    </div>
+    </>
+  );
+}

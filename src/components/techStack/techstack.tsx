b6bf2be77000c6afb40317d73f90a07a5f8ccc0b
Mabroor Ahmed <mabroorahmed18@gmail.com>
2024-02-24 17:41:52 +0000

    layout fixed for techstack

--- src/components/techStack/techstack.tsx
+++ src/components/techStack/techstack.tsx
@@ -1,8 +1,9 @@
 import { FaReact, FaNodeJs } from "react-icons/fa";
 import { DiMysql } from "react-icons/di";
-import { BiLogoSpringBoot, BiLogoKubernetes } from "react-icons/bi";
+import { BiLogoSpringBoot, BiLogoKubernetes, BiLogoGoogleCloud } from "react-icons/bi";
 import { GrDocker } from "react-icons/gr";
 import "./techstack.css";
+import { TbBrandDjango } from "react-icons/tb";
 
 export default function TechStack() {
   return (
@@ -12,7 +13,7 @@ export default function TechStack() {
           <h5>Front-End</h5>
           <div className="technologies">
             <div className="react">
-              <FaReact size={25} style={{ background: "transparent" }} /> React
+              <FaReact size={30} style={{ background: "transparent" }} /> React
             </div>
           </div>
         </div>
@@ -20,37 +21,56 @@ export default function TechStack() {
           <h5>Back-End</h5>
           <div className="technologies">
             <div className="nodejs">
-              <FaNodeJs size={25} style={{ background: "transparent" }} />{" "}
+              <FaNodeJs size={30} style={{ background: "transparent" }} />{" "}
               Nodejs
             </div>
             <div className="springboot">
               <BiLogoSpringBoot
-                size={25}
+                size={30}
                 style={{ background: "transparent" }}
               />{" "}
               Spring Boot
+            </div>
+            <div className="nodejs">
+              <TbBrandDjango size={30} style={{ background: "transparent" }} />{" "}
+              Django
             </div>
           </div>
         </div>
         <div className="database section subtitle">
           <h5>Database</h5>
-          <div className="mysql">
-            <DiMysql size={25} style={{ background: "transparent" }} /> My
-            <span className="sql">SQL</span>
+          <div className="technologies">
+            <div className="mysql">
+              <DiMysql size={30} style={{ background: "transparent" }} /> My
+              <span className="sql">SQL</span>
+            </div>
           </div>
         </div>
         <div className="other section subtitle">
           <h5>Other</h5>
-          <div className="docker">
-            <GrDocker size={25} style={{ background: "transparent" }} /> Docker
-          </div>
-          <div className="kubernetes">
-            <BiLogoKubernetes size={25} style={{ background: "transparent" }} />
-            Kubernetes
+          <div className="technologies">
+            <div className="docker">
+              <GrDocker size={30} style={{ background: "transparent" }} />{" "}
+              Docker
+            </div>
+            <div className="kubernetes">
+              <BiLogoKubernetes
+                size={30}
+                style={{ background: "transparent" }}
+              />
+              Kubernetes
+            </div>
           </div>
         </div>
         <div className="learning-interest section subtitle">
           <h5>Learning interest</h5>
+          <div className="technologies">
+            <BiLogoGoogleCloud
+              className="gcp"
+              size={30}
+              style={{ background: "transparent" }}
+            />
+          </div>
         </div>
       </div>
     </>
